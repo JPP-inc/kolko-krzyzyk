56dbeaca9ea477a5b668f64cafe07d0561d921df
Nikitaaa-P <n.pilarski@zs2wagrowiec.pl>
2025-04-03 12:02:15 +0200

    Update gra.py

--- gra.py
+++ gra.py
@@ -7,7 +7,7 @@ def check_winner(board, player):
     for row in board:
         if all(cell == player for cell in row):
             return True
-    
+            
     for col in range(3):
         if all(board[row][col] == player for row in range(3)):
             return True
